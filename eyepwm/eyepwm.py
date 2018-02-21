fdfcf4026b26c856a17c01109e06b010782b12ec
Harry Callahan <unsuitableduck@gmail.com>
2018-02-21 20:44:48 +0000

    First stab at writing the eyepwm package.

--- eyepwm/eyepwm.py
+++ eyepwm/eyepwm.py
@@ -4,52 +4,71 @@
 Created on Thu Feb  8 16:55:05 2018
 
 @author: Harry Callahan
+
+https://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code
+https://github.com/adafruit/Adafruit_Python_PCA9685
+https://learn.adafruit.com/adafruit-16-channel-pwm-servo-hat-for-raspberry-pi/overview
 """
 
+# from Adafruit_PCA9685 import PCA9685
+from Adafruit_PCA9685 import PCA9685
+import time
 import numpy
-from pyquaternion import *
 
 class eyepwm:
 
     # Constructor for the eye class
-    def __init__(self, radius, centre):
+    def __init__(self, radius, centre, vertical_channel=0, horizontal_channel=1):
 
-        # wlogger.log_info("Initialising Eye")
+        # PWM Setup Variables
+        self.eye_vert = PCA9685(0x40) # Always use the default i2c address for the PWM hats
+        self.eye_horiz = PCA9685(0x40)
+        self.eye_vert_setPWMFreq(50)
+        self.eye_horiz_setPWMFreq(50)
+        self.vert_channel = vertical_channel
+        self.horiz_channel = horizontal_channel
 
-        # Radius of eye
+        # Eye Position Tracking Variables (degrees)
+        self.eye_vert_angle = 0
+        self.eye_horiz_angle = 0
+        self.__eye_angle_max = 30
+        self.__eye_angle_min = -30
+
+        # 
+        
+        # Eye Simulation
         self.radius = radius
         self.centre = centre
 
-        # latitudes & longitudes
-        self.lats = 50
-        self.longs = 50
+    def step_vert_angle(self, step_size):
+        """Advances the eye position by the "step_size" argument"""
 
-        self.user_theta = 0
-        self.user_height = 0
-        self.x_angle_rad = 0
-        self.y_angle_rad = 0
-        self.angle_rad_limit = 0.7
+        # First calculate the new angle
+        self.eye_vert_angle += step_size
+        if self.eye_vert_angle > self.__eye_angle_max:
+            self.eye_vert_angle = self.__eye_angle_max
+        elif self.eye_vert_angle < -self.__eye_angle_min:
+            self.eye_vert_angle = -self.__eye_angle_min
 
-    def step_x(self, step_size):
+        # Now map the new angle against a correction matrix, which accounts for the mechanism of the eye.
 
-        self.x_angle_rad += step_size
+        # Convert the corrected angle into a PWM tick count and therefore duty cycle
+        pwm.setPWM(self.vert_channe, 0, conv_angle_tick(self.eye_vert_angle))
+        
 
-        if self.x_angle_rad > self.angle_rad_limit:
-            self.x_angle_rad = self.angle_rad_limit
-        elif self.x_angle_rad < -self.angle_rad_limit:
-            self.x_angle_rad = -self.angle_rad_limit
-        else:
-            rot_x = Quaternion(axis=[1.0, 0.0, 0.0], angle=step_size).normalised
-            self.current_quaternion *= rot_x
+    def step_horiz_angle(self, step_size):
 
-    def step_y(self, step_size):
+        self.eye_horiz_angle += step_size
+        if self.eye_horiz_angle > self.__eye_angle_max:
+            self.eye_horiz_angle = self.__eye_angle_max
+        elif self.eye_horiz_angle < -self.__eye_angle_min:
+            self.eye_horiz_angle = -self.__eye_angle_min
 
-        self.y_angle_rad += step_size
+    def conv_angle_ticks(self, angle):
+        """ Maps the angle of the eye position to a servo rotation (0 to 180) by outputting a tick count, 
+        where the fraction of ticks between 0 and 4096 defines the servo position. 
+        0% duty results in an angle of -60, while 100% (4096) results in an angle of 60. """
 
-        if self.y_angle_rad > self.angle_rad_limit:
-            self.y_angle_rad = self.angle_rad_limit
-        elif self.y_angle_rad < -self.angle_rad_limit:
-            self.y_angle_rad = -self.angle_rad_limit
-        else:
-            rot_y = Quaternion(axis=[0.0, 1.0, 0.0], angle=step_size).normalised
-            self.current_quaternion *= rot_y
+        scaler = 4096 / 120
+        ticks = 2048 + scaler*angle
+        return ticks
