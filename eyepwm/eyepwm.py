0b55806471d984cd4c569503b2ec9b992518e270
Harry Callahan <unsuitableduck@gmail.com>
2018-03-03 14:54:26 +0000

    Got the servomotors working reliably after I realised the control scheme was so arbitary. Test script moves it back and forwards at 1s intervals.

--- eyepwm/eyepwm.py
+++ eyepwm/eyepwm.py
@@ -10,7 +10,6 @@ https://github.com/adafruit/Adafruit_Python_PCA9685
 https://learn.adafruit.com/adafruit-16-channel-pwm-servo-hat-for-raspberry-pi/overview
 """
 
-# from Adafruit_PCA9685 import PCA9685
 from Adafruit_PCA9685 import PCA9685
 import time
 import numpy
@@ -18,15 +17,18 @@ import numpy
 class eyepwm:
 
     # Constructor for the eye class
-    def __init__(self, radius, centre, vertical_channel=0, horizontal_channel=1):
+    def __init__(self, radius, centre, vertical_channel = 0, horizontal_channel = 1):
 
         # PWM Setup Variables
-        self.eye_vert = PCA9685(0x40) # Always use the default i2c address for the PWM hats
-        self.eye_horiz = PCA9685(0x40)
-        self.eye_vert_setPWMFreq(50)
-        self.eye_horiz_setPWMFreq(50)
-        self.vert_channel = vertical_channel
-        self.horiz_channel = horizontal_channel
+        self.eye                          = PCA9685(0x40) # Always use the default i2c address for the PWM hats
+        self.eye_vert.set_pwm_freq(50)
+        self.eye_horiz.set_pwm_freq(50)
+        self.vert_ch                   = vertical_channel
+        self.horiz_ch                 = horizontal_channel
+
+        self.pos_1ms =        8e-4 / ( 20e-3 / 4096)
+        self.pos_1p5ms = 15e-4 / ( 20e-3 / 4096)
+        self.pos_2ms =      22e-4 / ( 20e-3 / 4096)               
 
         # Eye Position Tracking Variables (degrees)
         self.eye_vert_angle = 0
@@ -53,9 +55,9 @@ class eyepwm:
         # Now map the new angle against a correction matrix, which accounts for the mechanism of the eye.
 
         # Convert the corrected angle into a PWM tick count and therefore duty cycle
-        pwm.setPWM(self.vert_channe, 0, conv_angle_tick(self.eye_vert_angle))
-        
+        self.eye_vert.set_pwm(self.vert_channel, 0, self.conv_angle_ticks(self.eye_vert_angle))
 
+        
     def step_horiz_angle(self, step_size):
 
         self.eye_horiz_angle += step_size
@@ -72,3 +74,12 @@ class eyepwm:
         scaler = 4096 / 120
         ticks = 2048 + scaler*angle
         return ticks
+
+    def vert_test(self):
+        while True:
+            self.eye_vert.set_pwm(self.vert_channel, 0, int(self.pos_1ms))
+            time.sleep(1)
+            self.eye_vert.set_pwm(self.vert_channel, 0, int( self.pos_1p5ms))
+            time.sleep(1)
+            self.eye_vert.set_pwm(self.vert_channel, 0, int(self.pos_2ms))
+            time.sleep(2)
---
+++ eyepwm_tests.py
@@ -0,0 +1,12 @@
+import eyepwm
+import time
+
+x = eyepwm.eyepwm(0,0)
+
+# x.step_vert_angle(50)
+# time.sleep(1)
+# x.step_vert_angle(0)
+# time.sleep(1)
+# x.step_vert_angle(100)
+
+x.vert_test()
