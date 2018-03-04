248b1f9327d77b7943c00d723579f4a585d07696
Harry Callahan <unsuitableduck@gmail.com>
2018-03-04 10:02:47 +0000

    removed all private variables for easier debugging

--- eyepwm/eyepwm.py
+++ eyepwm/eyepwm.py
@@ -24,23 +24,23 @@ class eyepwm:
 
         # PWM Setup Variables
         self.eye                  = PCA9685(0x40) # Always use the default i2c address for the PWM hats
-        self.__vert_ch            = vertical_channel
-        self.__horiz_ch           = horizontal_channel
-        self.__pwm_freq = 50 
+        self.vert_ch            = vertical_channel
+        self.horiz_ch           = horizontal_channel
+        self.pwm_freq = 50 
         self.eye.set_pwm_freq(50)
 
-        self.__pwm_bitres = 4096
-        self.__pwm_scaler = self.__pwm_bitres * (1/self.__pwm_freq)
+        self.pwm_bitres = 4096
+        self.pwm_scaler = self.pwm_bitres * (1/self.pwm_freq)
         # The PWM periods for a typical servomotor is between 1ms and 2ms, with 1.5ms being the middle of the useable range.        
-        self.__pwm_period_min   = 10e-4 * self.__pwm_scaler # This is the minimum angle (1ms)
-        self.__pwm_period_zero  = 15e-4 * self.__pwm_scaler # This angle is the middle of the servo range (1.5ms)
-        self.__pwm_period_max   = 20e-4 * self.__pwm_scaler # This is the maximum angle (2ms)
-        self.__pwm_period_range = self.__pwm_period_max - self.__pwm_period_min
-        self.__servo_angle_max  =  80
-        self.__servo_angle_min  = -80
-        self.__servo_angle_range = self.__servo_angle_max - self.__servo_angle_min
-        self.__eye_angle_max    =  30
-        self.__eye_angle_min    = -30
+        self.pwm_period_min   = 10e-4 * self.pwm_scaler # This is the minimum angle (1ms)
+        self.pwm_period_zero  = 15e-4 * self.pwm_scaler # This angle is the middle of the servo range (1.5ms)
+        self.pwm_period_max   = 20e-4 * self.pwm_scaler # This is the maximum angle (2ms)
+        self.pwm_period_range = self.pwm_period_max - self.pwm_period_min
+        self.servo_angle_max  =  80
+        self.servo_angle_min  = -80
+        self.servo_angle_range = self.servo_angle_max - self.servo_angle_min
+        self.eye_angle_max    =  30
+        self.eye_angle_min    = -30
 
         # Servo Position Tracking Variables (degrees)
         self.eye_vert_angle   = 0
@@ -66,7 +66,7 @@ class eyepwm:
         servo_pwm_period = conv_servo_angle_ms(int(servo_angle))
 
         # Apply this new duty cycle to the servomotor
-        self.eye.set_pwm(self.__vert_ch, 0, servo_pwm_period)
+        self.eye.set_pwm(self.vert_ch, 0, servo_pwm_period)
 
         
     def step_horiz_angle(self, step_size):
@@ -74,7 +74,7 @@ class eyepwm:
         self.eye_horiz_angle = minmax(self.eye_horiz_angle, eye)
         servo_angle = map_eye_to_servo(self.eye_horiz_angle);
         servo_pwm_period = conv_servo_angle_ms(int(servo_angle))
-        self.eye.set_pwm(self.__horiz_ch, 0, servo_pwm_period)
+        self.eye.set_pwm(self.horiz_ch, 0, servo_pwm_period)
 
         
     def get_vert_angle(self):
@@ -88,8 +88,8 @@ class eyepwm:
         A typical servo positioning system moves between -90 and 90 degress with periods between 1 and 2 ms.
         However, this library automatically takes the input angle and scales it to a position between the max servo range variables """
 
-        scaled_angle = angle / self.__servo_angle_range
-        return self.__pwm_period_min + (scaled_angle * self.__pwm_period_range)
+        scaled_angle = angle / self.servo_angle_range
+        return self.pwm_period_min + (scaled_angle * self.pwm_period_range)
 
     
     def map_eye_to_servo(self, angle):
@@ -106,23 +106,23 @@ class eyepwm:
         """
 
         if eye_or_servo == eye :
-            if new_input > self.__eye_angle_max:
-                new_input = self.__eye_angle_max
-            elif new_input < self.__eye_angle_min:
-                new_input = self.__eye_angle_min
+            if new_input > self.eye_angle_max:
+                new_input = self.eye_angle_max
+            elif new_input < self.eye_angle_min:
+                new_input = self.eye_angle_min
         elif eye_or_servo == servo :
-            if new_input > self.__servo_angle_max:
-                new_input = self.__servo_angle_max
-            elif new_input < self.__servo_angle_min:
-                new_input = self.__servo_angle_min
+            if new_input > self.servo_angle_max:
+                new_input = self.servo_angle_max
+            elif new_input < self.servo_angle_min:
+                new_input = self.servo_angle_min
 
 
     def vert_test(self):
         while True:
-            self.eye_vert.set_pwm(self.__vert_ch, 0, int(self.__pwm_period_min))
+            self.eye_vert.set_pwm(self.vert_ch, 0, int(self.pwm_period_min))
             time.sleep(1)
-            self.eye_vert.set_pwm(self.__vert_ch, 0, int( self.__pwm_period_zero))
+            self.eye_vert.set_pwm(self.vert_ch, 0, int( self.pwm_period_zero))
             time.sleep(1)
-            self.eye_vert.set_pwm(self.__vert_ch, 0, int(self.__pwm_period_max))
+            self.eye_vert.set_pwm(self.vert_ch, 0, int(self.pwm_period_max))
             time.sleep(2)
 
